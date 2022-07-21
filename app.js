78ccf61dba0d623f9427628ef36f572f0802856f
Sancaralp Elmas <73790877+LeongardZ@users.noreply.github.com>
2022-07-21 21:53:58 +0300

    added sections

--- app.js
+++ app.js
@@ -12,5 +12,24 @@ function PageTrans(){
             this.className += 'active-btn';
         })
     }
+
+    //Sections active class
+    allSections.addEventListener('click',(e)=>{
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from other buttons
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 PageTrans();
